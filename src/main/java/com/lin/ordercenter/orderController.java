6dc33fd76223a6e634d2922c3ffde0975383c362
中科软-林雪峰 <linxuefeng@sinosoft.com.cn>
2021-06-09 13:16:39 +0800

    Lin: 增加接口

--- src/main/java/com/lin/ordercenter/orderController.java
+++ src/main/java/com/lin/ordercenter/orderController.java
@@ -1,5 +1,15 @@
 package com.lin.ordercenter;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class orderController {
 
+  /**
+   * @author linxuefeng
+   * @function 投保
+   */
+  public void monthRelease(String request) {
+
+  }
 }
